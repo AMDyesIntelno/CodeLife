dbfdaef6c8181348ee54ecb47b9eb62df28e6058
AMDyesIntelno <2621023869@qq.com>
2020-05-27 16:15:40 +0800

    update

---
+++ 数据结构(瞎写)/Sort/selection_sort.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+void selection_sort(int *array,int len){//选择排序
+    for(int i=0;i<len;++i){
+        int posi=i;//记录每一次检索的最小值的位置
+        for(int j=i+1;j<len;++j){
+            if(array[j]<array[posi]){//更新最小值的位置
+                posi=j;
+            }
+        }
+        swap(array[posi],array[i]);
+    }
+    return;
+}
+void print(int *array,int len){
+    for(int i=0;i<len;++i){
+        cout<<array[i]<<' ';
+    }
+    cout<<endl;
+    return;
+}
+int main(){
+    int array[]={1321,5649,98,41,518,9,41};
+    int len=sizeof(array)/sizeof(array[0]);
+    selection_sort(array,len);
+    print(array,len);
+    return 0;
+}
